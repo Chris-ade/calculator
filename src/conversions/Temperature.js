062376ceb748c21159ecbd0043a65ca47c06c118
Chris Adebiyi <christopherolatunjiade@gmail.com>
2024-01-22 02:29:28 +0100

    Fixed and remove unnecessary conversion from the Temperature category

--- src/conversions/Temperature.js
+++ src/conversions/Temperature.js
@@ -2,8 +2,6 @@ const conversionData = {
     c: { label: 'C', title: 'Celsius' },
     f: { label: 'F', title: 'Fahrenheit' },
     k: { label: 'K', title: 'Kelvin' },
-    r: { label: 'R', title: 'Rankine' },
-    re: { label: 'Re', title: 'Reaumur' }
 };
 
 const calculateResult = (setSecondScreenValue, unit1, unit2, value) => {
@@ -16,64 +14,22 @@ const calculateResult = (setSecondScreenValue, unit1, unit2, value) => {
     } else {
       switch (condition) {
         case ('c-f'):
-          result = value * 33.8;
+          result = (value * (9 / 5)) + 32;
         break;
         case ('c-k'):
-          result = value * 274.15;
-        break;
-        case ('c-r'):
-          result = value * 493.47; 
-        break;
-        case ('c-re'):
-          result = value * 0.8;
+          result = value + 273.15;
         break;
         case ('f-c'):
-          result = value / 33.8;
+          result = (value - 32) * (5 / 9);
         break;
         case ('f-k'):
-          result = value * 255.927778;
-        break;
-        case ('f-r'):
-          result = value * 460.67;
-        break;
-        case ('f-re'):
-          result = value * -13.7777778;
+          result = (value - 32) * (5 / 9) + 273.15;
         break;
         case ('k-c'):
-          result = value * (-272.15);
+          result = value - 273.15;
         break;
         case ('k-f'):
-          result = value * (-457.87);
-        break;
-        case ('k-r'):
-          result = value * 1.8;
-        break;
-        case ('k-re'):
-          result = value * (-217.72);
-        break;
-        case ('r-c'):
-          result = value * (-272.594444);
-        break;
-        case ('r-f'):
-          result = value * (-458.67);
-        break;
-        case ('r-k'):
-          result = value * 0.555555556;
-        break;
-        case ('r-re'):
-          result = value * (-218.075556);
-        break;
-        case ('re-c'):
-          result = value * 1.25;
-        break;
-        case ('re-f'):
-          result = value * 34.25;
-        break;
-        case ('re-k'):
-          result = value * 274.4;
-        break;
-        case ('re-r'):
-          result = value * 493.92;
+          result = (value - 273.15) * (9 / 5) + 32;
         break;
         default:
           result = "Invalid conversion";
